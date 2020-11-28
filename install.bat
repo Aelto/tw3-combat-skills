9e8071ede7e71699046f75786ef42b6f368a2bf4
Aelto <Aelto@users.noreply.github.com>
2020-11-28 11:44:47 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 
 call variables.cmd
 
-rmdir "%gamepath%\mods\%modname%\content\scripts"
+rmdir "%gamepath%\mods\%modname%\content\scripts" /s /q
 XCOPY "%modpath%\src\local\" "%gamePath%\mods\%modname%\content\scripts\%modname%_local\" /e /s /y
 XCOPY "%modpath%\strings\" "%gamePath%\mods\%modname%\content\" /e /s /y
 copy "%modpath%\mod-menu.xml" "%gamePath%\bin\config\r4game\user_config_matrix\pc\%modname%.xml" /y
